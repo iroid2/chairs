5c2123fdea6834e8fddcaa94d315965f63f949ce
TUKAHEEBWARUHANGA IRAD <104001652+iroid2@users.noreply.github.com>
2023-04-10 11:26:26 +0300

    .

---
+++ hiddenMen.js
@@ -0,0 +1,7 @@
+const menuIcon =document.querySelector(".menuIcon");
+const cancelBtn =document.querySelector(".cancelBtn");
+const homeNav =document.querySelector(".homeNav");
+
+menuIcon.addEventListener(('click'),function(){
+    console.log("now clicked")
+})
